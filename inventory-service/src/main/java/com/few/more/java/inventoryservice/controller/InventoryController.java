a6d260d72db7e546587335876d00a709368309d2
kamlesh.stech <kamlesh.stech@gmail.com>
2023-03-22 21:14:10 +0530

    chnages done

--- inventory-service/src/main/java/com/few/more/java/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/few/more/java/inventoryservice/controller/InventoryController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+//
 @RestController
 @RequestMapping("/api/inventory")
 public class InventoryController {
